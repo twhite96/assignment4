4ff3dd5f579f005d03ab6a5786702845cab649f9
Tiffany White <tiffanywhitedev412@gmail.com>
2016-11-29 00:20:29 -0500

    NOW I UNDERSTAND. But I still can't get it to work

--- assignment4.js
+++ assignment4.js
@@ -1,11 +1,7 @@
-// The anonymous function below will fire on page load
-
 (function() {
-  // Magic!
   console.log('Keepin\'n it clean with an external script!');
 
-  var $url  = $('.flexsearch-input').val() !== '' || 'http://www.mattbowytz.com/simple_api.json?data=all';
-
+  var $url = $('.flexsearch-input').val() || 'http://www.mattbowytz.com/simple_api.json?data=programming';
   $('#mainForm').on('submit', function(e) {
     e.preventDefault();
     $.ajax({
@@ -13,8 +9,16 @@
       method: 'GET'
     }).success(function(data) {
       console.log(data);
-    }).fail(function(data) {
-      console.log(data);
+      var programmingArray = data.data.programming;
+      var lastString = ' ';
+      $.each(programmingArray, function( i ) {
+        JSON.stringify(lastString[i]);
+      });
     });
+
+    //loop through the array:
+
+  }).fail(function(data) {
+    console.log(data);
   });
-})();
+}());
